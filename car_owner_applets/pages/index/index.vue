b7215bc700ddd17d303a02aca2e9c5c09a0b5072
1499033843@qq.com <1499033843@qq.com>
2020-08-12 15:56:45 +0800

    add

--- car_owner_applets/pages/index/index.vue
+++ car_owner_applets/pages/index/index.vue
@@ -1,9 +1,8 @@
 <template>
 	<view class="prohibition">
 		<view class="nav-top" :style="'height:' +  nav.height + 'px;' + 'padding-top:' + nav.top + 'px;padding-bottom:10rpx'">
-			<view class="left" :style="'top:' + nav.top + 'px'">
-				<view class="iconfont icon-xiaoxi">111</view>
-			</view>
+			<view class="iconfont iconsaoyisao"></view>
+			<view class="iconfont icondingwei margin-left"></view>
 			<view class="nav-top-text">宁波保险科技产业园</view>
 		</view>
 		<view>
@@ -34,31 +33,27 @@
 	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 	.nav-top {
 		position: relative;
 		display: flex;
 		align-items: center;
-		background: #fff;
+		background: #242328;
 		font-size: 26rpx;
-
-		.left {
-			float: left;
-			position: absolute;
-			width: max-content;
-			height: max-content;
-			top: 0;
-			bottom: 0;
-			left: 20rpx;
-			margin: auto;
-
-			.iconfont {
-				color: #3C3C3C;
-			}
+		padding-left:30rpx;
+		.iconsaoyisao {
+			color: #fff;
+			font-size:38rpx;
 		}
-
+        .margin-left{
+			margin-left:15rpx;
+			margin-right:15rpx;
+			color: #fff;
+			font-size:38rpx;
+		}
 		.nav-top-text {
-			padding-left: 80rpx;
+			color:#fff;
+			padding-left:0 !important
 		}
 	}
 </style>
