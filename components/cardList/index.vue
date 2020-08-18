39ee10f55749519afe1946b7eec8fa2c1d6ed31c
1499033843@qq.com <1499033843@qq.com>
2020-08-18 16:27:09 +0800

    add

---
+++ components/cardList/index.vue
@@ -0,0 +1,80 @@
+<template>
+		<view  class="card flex-c-between">
+			<view class="card-left">
+				<view class="flex-c-center about-card">
+					<image src="/static/img/baoxian.png" class="card-logo"></image>
+					<view class="card-title flex-c-between">
+						<view>阳光保险</view>
+						<view>12次洗车卡</view>
+					</view>
+				</view>
+				<view class="card-num">NO.456456456</view>
+			</view>
+			<view class="card-right">
+				<view class="coupon-top">2020.07.15到期</view>
+				<view class="coupon-shop coupon-margin">
+					适用商家
+				</view>
+				<view class="coupon-shop">
+					立即使用
+				</view>
+			</view>
+		</view>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+.card{
+		width: 94%;
+		padding:20rpx 0;
+		background: #333333;
+		margin:0 auto;
+		.about-card{
+			
+			.card-logo{
+				width: 66rpx;
+				height: 66rpx;
+			}
+			.card-title{
+				font-size:28rpx;
+				color:rgba(255,255,255,.6);
+				margin-left:20rpx;
+				flex-direction: column;
+			}
+		}
+		.card-left{
+			margin-left:30rpx;
+		}
+		.card-num{
+			color:#A2A2A2;
+			font-size:24rpx;
+			margin-top:20rpx
+		}
+		.card-right{
+			margin-right:30rpx;
+			display: flex;
+			flex-direction: column;
+			align-items: flex-end;
+			.coupon-shop{
+				width: 154rpx;
+				height: 60rpx;
+				line-height: 58rpx;
+				border-radius: 34rpx;
+				color:$theme-color;
+				font-size:24rpx;
+				border:2rpx solid $theme-color;
+				text-align: center;
+			}
+			.coupon-margin{
+						 margin-bottom:15rpx;
+			}
+			.coupon-top{
+				margin-bottom:20rpx;
+				color:#A2A2A2;
+				font-size: 24rpx;
+			}
+		}
+	}
+</style>
---
+++ components/myCoupon/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<view>
+		<view class="coupon flex-c-between">
+			<view class="coupon-left">
+				<view class="coupon-price">阳光保险洗车券</view>
+				<view class="coupon-use">可用门店，全网通用</view>
+				<view class="coupon-time">2020.07.07-2020.07.21</view>
+			</view>
+			<view class="coupon-right">
+				<view class="coupon-shop coupon-margin">适用商家</view>
+				<view class="coupon-shop">立即使用</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style  lang="scss" scoped>
+	.coupon{
+		width: 94%;
+		height: 198rpx;
+		margin:20rpx auto;
+		background: url('../../static/img/couponBg.png') no-repeat center center;
+	   background-size:100%;
+	   .coupon-left{
+		   margin-left:40rpx;
+		   color:$white-color;
+		   .coupon-price{
+			   font-size:32rpx;
+			    margin-bottom:15rpx;
+		   }
+		   .coupon-use{
+			   font-size:24rpx;
+			   margin-bottom:10rpx;
+		   }
+		   .coupon-time{
+			   font-size:22rpx;
+		   }
+	   }
+	   .coupon-right{
+		   width: 28%;
+		   height: 198rpx;
+		   display: flex;
+		   align-items: center;
+		   justify-content: center;
+		   flex-direction: column;
+		 .coupon-shop{
+			 width: 154rpx;
+			 height: 60rpx;
+			 line-height: 58rpx;
+			 border-radius: 34rpx;
+			 color:$theme-color;
+			 font-size:24rpx;
+			 border:2rpx solid $theme-color;
+			 text-align: center;
+		 }
+		 .coupon-margin{
+			 margin-bottom:15rpx;
+		 }
+		   
+	   }
+	}
+</style>
