32134bf6beb2b4c250ee25b35edbff7f83309456
1499033843@qq.com <1499033843@qq.com>
2020-08-19 10:38:56 +0800

    AD

---
+++ userPage/loseCard.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<view class="shop">
+			<view class="shopName">阳光保险</view>
+			<card-list></card-list>
+		</view>
+	</view>
+</template>
+
+<script>
+	import cardList from '@/components/cardList/index.vue'
+	export default{
+		components: {
+			cardList
+		},
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.shopName{
+		color:#fff;
+		padding:15rpx 30rpx 30rpx 30rpx;
+	}
+</style>
