edb0263d70ed62169754509b7aefde6c7b5e4e15
muliu <203013674@qq.com>
2020-09-22 17:28:28 +0800

    小程序-在线预约(门店，增值服务)，日历组件修改，——沈列哲

---
+++ makeAppoint/appiontInsurance.vue
@@ -0,0 +1,152 @@
+<template>
+	<view class="appiontInsurance">
+		<view class="insurance">
+			<view class="insurance_msg">
+				<icon class="iconfont icondianpu"></icon>
+				<view class="insurance_shop">阳光保险</view>
+			</view>
+			<view class="msg_name">阳光保险增值服务名称</view>
+		</view>
+		<view class="insurance_temp">可选套餐</view>
+		<view class="insurance_list">
+			<view class="list_card">
+				<view class="card_msg">
+					<view class="temp_name">套餐A</view>
+					<view class="temp_msg">指定门店 &nbsp;&nbsp;&nbsp; 剩余次数：8</view>
+				</view>
+				<view class="card_choose">
+					<image :src="isChoose"></image>
+				</view>
+			</view>
+			<view class="list_card">
+				<view class="card_msg">
+					<view class="temp_name">套餐B</view>
+					<view class="temp_msg">全网通用&nbsp;&nbsp;&nbsp; 剩余次数：1</view>
+				</view>
+				<view class="card_choose">
+					<image :src="isChoose"></image>
+				</view>
+			</view>
+			<view class="list_card">
+				<view class="card_msg">
+					<view class="temp_name">套餐C</view>
+					<view class="temp_msg">ָ全网通用 &nbsp;&nbsp;&nbsp; 剩余次数：1</view>
+				</view>
+				<view class="card_choose">
+					<image :src="isChoose"></image>
+				</view>
+			</view>
+		</view>
+		<button class="insurance_btn">下一步</button>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				unChoose:require('../static/img/unget.png'),
+				isChoose:require('../static/img/unget.png'),
+				choosed:require('../static/img/get.png'),
+			}
+		},
+		methods:{
+		},
+		mounted() {
+		}
+	}
+</script>
+
+<style lang="scss" scope>
+.appiontInsurance{
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	.insurance{
+		display: flex;
+		flex-direction: column;
+		padding: 16rpx 30rpx;
+		padding-bottom: 50rpx;
+		background-color: #353439;
+		margin-bottom: 30rpx;
+		.insurance_msg{
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			margin-bottom: 10rpx;
+			icon {
+				font-size: 48rpx;
+				color: #ffffff;
+				opacity: 0.8;
+			}
+			.insurance_shop {
+				font-size: 24rpx;
+				color: #ffffff;
+				opacity: 0.8;
+			}
+		}
+		.msg_name {
+				color: #ffffff;
+				font-size: 36rpx;
+			}
+	}
+	.insurance_temp{
+		background-color: #353439;
+		margin-bottom: 30rpx;
+		font-size: 30rpx;
+		color: #EEAB68;
+		padding: 16rpx 30rpx;
+		margin-bottom: 6rpx;
+	}
+	.insurance_list{
+		display: flex;
+		flex-direction: column;
+		background-color: #353439;
+		padding: 16rpx 30rpx;
+		.list_card{
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			align-items: center;
+			border-bottom: 2rpx solid #242328;
+			padding: 20rpx 0rpx;
+			.card_msg{
+				display: flex;
+				flex-direction: column;
+				.temp_name{
+					font-size: 36rpx;
+					color: #FFFFFF;
+					margin: 5rpx 0rpx;
+				}
+				.temp_msg{
+					font-size: 26rpx;
+					color: #9A9A9A;
+					margin: 5rpx 0rpx;
+				}
+			}
+			.card_choose{
+				width: 44rpx;
+				height: 44rpx;
+				image{
+					width: 100%;
+					height: 100%;
+				}
+			}
+		}
+	}
+	.insurance_btn{
+		position: absolute;
+		width: 652rpx;	
+		left: 0;
+		right: 0;
+		margin: auto; 
+		bottom: 50rpx;
+		background: #FF6F00;
+		border-radius: 44rpx;
+		font-size: 32rpx;
+		color: #FFFFFF;
+		opacity: 0.8;
+	}
+}
+</style>
--- makeAppoint/appiontOnline.vue
+++ makeAppoint/appiontOnline.vue
@@ -6,11 +6,17 @@
 					<icon class="iconfont icondianpu"></icon>
 					<view class="shop_name">百慕大汽修厂</view>
 				</view>
-				<view class="top_price">￥<view style="display: contents;">199.9</view></view>
+				<view class="top_price">
+					￥
+					<view style="display: contents;">199.9</view>
+				</view>
 			</view>
 			<view class="top">
 				<view class="top_temp">暑期倾情让利套餐</view>
-				<view class="price">￥<view style="display: contents;font-size: 48rpx;">9.9</view></view>
+				<view class="price">
+					￥
+					<view style="display: contents;font-size: 48rpx;">9.9</view>
+				</view>
 			</view>
 		</view>
 		<view class="online_cars">
@@ -20,7 +26,7 @@
 		<view class="online_car">
 			<view class="car_msg">
 				<view class="car_name">一汽大众宝来</view>
-				<view class="car_config">大众  宝来  2020款  1.4TSI  双离合  28  大众  宝来  2020款  1.4TSI  双离合  28</view>
+				<view class="car_config">大众 宝来 2020款 1.4TSI 双离合 28 大众 宝来 2020款 1.4TSI 双离合 28</view>
 			</view>
 			<view class="car_img"><image src="../static/img/get.png" mode=""></image></view>
 		</view>
@@ -28,76 +34,129 @@
 			<view class="shops_title">选择门店</view>
 			<view class="shops_more">其他></view>
 		</view>
+		<view class="online_shop">
+			<view class="shop_name">品牌连锁的某区域门店</view>
+			<view class="shop_time">营业中 8:30-20:30</view>
+			<view class="shop_address">钱湖北路199号(巴丽新地旁边)</view>
+		</view>
+		<view class="online_calendar">
+			<ei-calendar 
+			:visible.sync="show" 
+			type="single" 
+			:disabledDate="disabledDate" 
+			v-model="date" 
+			:custom-date="customDate" 
+			format="YYYY-MM-DD"
+			:drawer="false">
+			</ei-calendar>
+		</view>
+		<view class="online_num">
+			<view class="num">上午已预约：50</view>
+			<view class="num">下午已预约：50</view>
+		</view>
+		<view class="online_note">
+			<view class="note_title">备注</view>
+			<view class="note_add">添加备注></view>
+		</view>
+		<view class="online_btn">
+			<button class="off">预约上午</button>
+			<button class="on">预约下午</button>
+		</view>
 	</view>
 </template>
 
 <script>
+import EiCalendar from '@/components/ei-calendar/ei-calendar';
+
+    export default {
+        components: {EiCalendar},
+        data() {
+            return {
+                show: true,
+				disDate:['2020/9/27','2020/9/28','2020/10/01'],
+                customDate: [{
+                    cellClass: 'custom-cell',
+                    date: '2019-05-23',
+                }]
+            }
+        },
+        methods: {
+            disabledDate(date) {
+                const start = new Date().getTime();
+                const end = new Date('2120/6/21').getTime();
+				this.disDate = this.disDate.map((item,index) => {
+					return new Date(item).getTime()
+				})
+				return date.getTime() <= start || date.getTime() >= end || date.getTime() == this.disDate[0]
+            }
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
-.appiontOnline{
+.appiontOnline {
 	display: flex;
 	flex-direction: column;
-	.online_top{
+	.online_top {
 		display: flex;
 		flex-direction: column;
 		padding: 10rpx 30rpx;
 		background-color: #353439;
 		margin-bottom: 30rpx;
-		.top{
+		.top {
 			display: flex;
 			flex-direction: row;
 			justify-content: space-between;
-			.top_shop{
+			.top_shop {
 				display: flex;
 				flex-direction: row;
 				align-items: center;
 				margin-bottom: 10rpx;
-				icon{
+				icon {
 					font-size: 48rpx;
-					color: #FFFFFF;
+					color: #ffffff;
 					opacity: 0.8;
 				}
-				.shop_name{
+				.shop_name {
 					font-size: 24rpx;
-					color: #FFFFFF;
+					color: #ffffff;
 					opacity: 0.8;
 				}
 			}
-			.top_price{
+			.top_price {
 				text-decoration: line-through;
 				font-size: 24rpx;
-				color: #FFFFFF;
+				color: #ffffff;
 				opacity: 0.6;
 			}
-			.top_temp{
-				color: #FFFFFF;
-				font-size:36rpx;
+			.top_temp {
+				color: #ffffff;
+				font-size: 36rpx;
 			}
-			.price{
+			.price {
 				font-size: 24rpx;
-				color: #FF6F00;
+				color: #ff6f00;
 			}
 		}
 	}
-	.online_cars{
+	.online_cars {
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
 		background-color: #353439;
 		padding: 16rpx 30rpx;
 		margin-bottom: 2rpx;
-		.cars_title{
-			color: #EEAB68;
+		.cars_title {
+			color: #eeab68;
 			font-size: 28rpx;
 		}
-		.cars_more{
+		.cars_more {
 			font-size: 24rpx;
-			color: #FFFFFF;
+			color: #ffffff;
 			opacity: 0.6;
 		}
 	}
-	.online_car{
+	.online_car {
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
@@ -105,49 +164,123 @@
 		background-color: #353439;
 		padding: 16rpx 30rpx;
 		margin-bottom: 30rpx;
-		.car_msg{
+		.car_msg {
 			display: flex;
 			flex-direction: column;
-			.car_name{
+			.car_name {
 				font-size: 32rpx;
-				color: #FFFFFF;
+				color: #ffffff;
 				margin-bottom: 20rpx;
 			}
-			.car_config{
+			.car_config {
 				width: 600rpx;
 				font-size: 24rpx;
-				color: #9A9A9A;
+				color: #9a9a9a;
 				overflow: hidden;
-				text-overflow:ellipsis;
+				text-overflow: ellipsis;
 				white-space: nowrap;
 			}
 		}
-		.car_img{
+		.car_img {
 			width: 44rpx;
 			height: 44rpx;
-			image{
+			image {
 				width: 100%;
 				height: 100%;
 			}
 		}
 	}
-	.online_shops{
+	.online_shops {
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
 		background-color: #353439;
 		padding: 16rpx 30rpx;
 		margin-bottom: 2rpx;
-		.shops_title{
-			color: #EEAB68;
+		.shops_title {
+			color: #eeab68;
 			font-size: 28rpx;
 		}
-		.shops_more{
+		.shops_more {
+			font-size: 24rpx;
+			color: #ffffff;
+			opacity: 0.6;
+		}
+	}
+	.online_shop {
+		display: flex;
+		flex-direction: column;
+		background-color: #353439;
+		padding: 16rpx 30rpx;
+		margin-bottom: 30rpx;
+		.shop_name {
+			font-size: 36rpx;
+			color: #eeab68;
+		}
+		.shop_time {
+			font-size: 24rpx;
+			color: #ffffff;
+			opacity: 0.8;
+			margin: 15rpx 0rpx;
+		}
+		.shop_address {
+			font-size: 24rpx;
+			color: #ffffff;
+			opacity: 0.8;
+		}
+	}
+	.online_calendar {
+		background-color: #353439;
+		padding: 16rpx 30rpx;
+	}
+	.online_num{
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		padding: 16rpx 30rpx;
+		background-color: #353439;
+		margin-bottom: 30rpx;
+		font-size: 32rpx;
+		color: #FFFFFF;
+		opacity: 0.8;
+	}
+	.online_note{
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		padding: 16rpx 30rpx;
+		background-color: #353439;
+		.note_title{
+			font-size: 28rpx;
+			color: #EEAB68;
+		}
+		.note_add{
 			font-size: 24rpx;
 			color: #FFFFFF;
 			opacity: 0.6;
 		}
 	}
+	.online_btn{
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		padding: 30rpx 30rpx;
+		.on{
+			width: 320rpx;
+			background-color: #FF6F00;
+			border-radius: 44rpx;
+			font-size: 32rpx;
+			opacity: 1;
+			color: #FFFFFF;
+		}
+		.off{
+			width: 320rpx;
+			background-color: #636363;
+			border-radius: 44rpx;
+			font-size: 32rpx;
+			opacity: 1;
+			color: #8B8B8B;
+		}
+	}
 }
-
 </style>
